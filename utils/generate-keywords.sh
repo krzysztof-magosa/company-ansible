9264feffdee905a7e531813faae0eeaa39ac00b9
Krzysztof Magosa <krzysztof@magosa.pl>
2016-09-05 21:41:17 +0200

    get keywords from 2.1 version of ansible

--- utils/generate-keywords.sh
+++ utils/generate-keywords.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
+ANSIBLE_VERSION=stable-2.1
+
 mkdir -p ./repos/
-[ ! -d ./repos/core/ ] && git clone https://github.com/ansible/ansible-modules-core.git ./repos/core/
-[ ! -d ./repos/extras/ ] && git clone https://github.com/ansible/ansible-modules-extras.git ./repos/extras/
+[ ! -d ./repos/core/ ] && git clone https://github.com/ansible/ansible-modules-core.git --branch=$ANSIBLE_VERSION ./repos/core/
+[ ! -d ./repos/extras/ ] && git clone https://github.com/ansible/ansible-modules-extras.git --branch=$ANSIBLE_VERSION ./repos/extras/
 
 ./generator.rb './repos/*/*/*.py'
